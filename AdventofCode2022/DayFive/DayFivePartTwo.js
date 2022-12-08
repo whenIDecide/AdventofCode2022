93777eb5ef48421ec3ab4edd66d73a178eef2d3f
Matthew Farr <61640210+whenIDecide@users.noreply.github.com>
2022-12-07 19:39:48 -0800

    Add files via upload

---
+++ AdventofCode2022/DayFive/DayFivePartTwo.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const moves = fs.readFileSync("boxes.txt", "utf8")
+.replace(/move (\d+) from (\d+) to (\d+)(\r)?/g,"$1 $2 $3")
+.split("\n")
+.map(function(item) {
+    return item.split(" ")
+});
+
+
+let stacks = [
+    ["R","H","M","P","Z"],
+    ["B","J","C","P"],
+    ["D","C","L","G","H","N","S"],
+    ["L","R","S","Q","D","M","T","F"],
+    ["M","Z","T","B","Q","P","S","F"],
+    ["G","B","Z","S","F","T"],
+    ["V","R","N"],
+    ["M","C","V","D","T","L","G","P"],
+    ["L","M","F","J","N","Q","W"]
+];
+
+function mover(number, from, to) {
+    let temp = from.splice(0, (number));
+    for (let i = 0; i < number; i++) {
+      to.unshift(temp[(number -1) - i]);
+    }
+  };
+  
+  let stacksAfterMoves = moves.forEach(el => {
+       mover(parseInt(el[0], 10)
+       ,stacks[parseInt(el[1], 10) -1]
+       ,stacks[parseInt(el[2], 10) -1])
+  });
+  
+  console.log(stacks)
